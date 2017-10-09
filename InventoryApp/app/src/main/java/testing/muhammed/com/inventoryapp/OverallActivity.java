c31176a8a0ede2cbdcaaaaaf30c61355e65fafab
muhammed <muhammed.thasneem@yahoo.com>
2017-10-09 11:53:37 +0530

    Initial commit

---
+++ InventoryApp/app/src/main/java/testing/muhammed/com/inventoryapp/OverallActivity.java
@@ -0,0 +1,13 @@
+package testing.muhammed.com.inventoryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OverallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overall);
+    }
+}
